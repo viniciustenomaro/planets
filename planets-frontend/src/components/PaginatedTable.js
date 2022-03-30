15c9345778821efa912c543dc163387070795aff
viniciustenomaro <vinicius.tenomaro@qyon.com>
2022-03-30 08:46:49 -0300

    implementing get planet by id

--- planets-frontend/src/components/PaginatedTable.js
+++ planets-frontend/src/components/PaginatedTable.js
@@ -1,171 +1,207 @@
-import { useState, useEffect } from 'react';
-import { useTheme } from '@mui/material/styles';
+import { useState, useEffect } from "react";
+import { useTheme } from "@mui/material/styles";
 import axios from "axios";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import Table from '@mui/material/Table';
-import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import Typography from "@mui/material/Typography";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170, align: "center" },
-    { id: 'population', label: 'Population', minWidth: 100, align: "center" },
-    { id: 'information' }
+  { id: "name", label: "Name", minWidth: 170, align: "center" },
+  { id: "population", label: "Population", minWidth: 100, align: "center" },
+  { id: "information" },
 ];
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function TablePaginationActions(props) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-    const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
-    };
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
 
-    const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
-    };
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
 
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+    </Box>
+  );
 }
 
 export default function CustomPaginationActionsTable() {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-    const [rows, setRows] = useState([]);
-    const [itemData, setItemData] = useState();
-    const [open, setOpen] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState([]);
+  const [itemData, setItemData] = useState();
+  const [open, setOpen] = useState(false);
 
-    const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setItemData(null);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+    setOpen(false);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleOpen = (url) => {
+    const fetch = async () => {
+      const result = await axios(url);
+
+      setItemData(result.data);
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
+    fetch();
+
+    setOpen(true);
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await axios(
+        `https://localhost:5001/api/planets?page=${page + 1}`
+      );
+
+      setRows(result.data);
     };
 
-    const handleClick = (url) => {
-        const fetch = async () => {
-            const result = await axios(
-                url
-            );
+    fetch();
+  }, [page]);
 
-            setItemData(result.data);
-        };
-
-        fetch();
-
-        setOpen(true);
-    }
-
-    useEffect(() => {
-        const fetch = async () => {
-            const result = await axios(
-                `https://localhost:5001/api/planets?page=${page + 1}`
-            );
-
-            setRows(result.data);
-        };
-
-        fetch();
-    }, [page]);
-
-    console.log(open)
-
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-                <TableHead>
-                    <TableRow>
-                        {columns.map((column) => (
-                            <TableCell
-                                key={column.id}
-                                align={column.align}
-                                style={{ minWidth: column.minWidth }}
-                            >
-                                {column.label}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row" align="center">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="center">
-                                {row.population}
-                            </TableCell>
-                            <TableCell align="center">
-                                <Button onClick={() => handleClick(row.url)}>
-                                    <VisibilityIcon>
-                                        <Modal
-                                            open={open}
-                                            onClose={handleClose}
-                                            aria-labelledby="modal-modal-title"
-                                            aria-describedby="modal-modal-description"
-
-                                        >
-                                            <div>Text</div>
-                                        </Modal>
-                                    </VisibilityIcon>
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination
-                            rowsPerPageOptions={[10]}
-                            colSpan={3}
-                            count={60}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            SelectProps={{
-                                inputProps: {
-                                    'aria-label': 'rows per page',
-                                },
-                                native: true,
-                            }}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            ActionsComponent={TablePaginationActions}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell
+                key={column.id}
+                align={column.align}
+                style={{ minWidth: column.minWidth }}
+              >
+                {column.label}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row" align="center">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.population}</TableCell>
+              <TableCell align="center">
+                <Button onClick={() => handleOpen(row.url)}>
+                  <VisibilityIcon></VisibilityIcon>
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[10]}
+              colSpan={3}
+              count={60}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  "aria-label": "rows per page",
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {itemData?.name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Diameter: {itemData?.diameter}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Climate: {itemData?.climate}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Gravity: {itemData?.gravity}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Terrain: {itemData?.terrain}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Population: {itemData?.population}
+          </Typography>
+        </Box>
+      </Modal>
+    </TableContainer>
+  );
 }
